0b4bca9d51c1f5823276050bef3a4d5df45523e8
Daniel Byrne <realdanielbyrne@icloud.com>
2019-11-21 00:00:03 -0600

    Update splitcsv.py

--- splitcsv.py
+++ splitcsv.py
@@ -4,7 +4,7 @@ if(len(sys.argv) < 2):
   print('Plase provide the csv file path to spilt')
   sys.exit()
 
-if(len(sys.argv) = 3):
+if(len(sys.argv) == 3):
   splitlevel = sys.argv[2]
 else:
   splitlevel = 5
@@ -14,10 +14,10 @@ csvfilename = open(fil, 'r', -1).readlines()
 file = 1
 
 #Number of lines to be written in new file
-record_per_file = int(11000000 - 500000)
+record_per_file = int(11000000/int(splitlevel))
 
 for j in range(len(csvfilename)):
   if j % record_per_file == 0:
     write_file = csvfilename[j:j+record_per_file]
-    open(str(fil)+ str(file) + '.tt.csv', 'w+').writelines(write_file)
+    open(str(fil)+ str(file) + '.csv', 'w+').writelines(write_file)
     file += 1
